18b9b638557a314e66b441490ca76bb28e481e13
geojorg <54997758+geojorg@users.noreply.github.com>
2019-09-16 09:07:18 -0500

    Initial commit

---
+++ Pokedex/Services/APIService.cs
@@ -0,0 +1,51 @@
+﻿namespace Pokedex
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Xamarin.Essentials;
+    using Xamarin.Forms;
+
+    public class APIService
+    {
+        public static void ConectivityStatus()
+        {
+            var current = Connectivity.NetworkAccess;
+            if (current == NetworkAccess.Internet)
+            {
+                //Internet Connection is Ok
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert(
+                    "No Internet Connection",
+                    "Please check your internet connection and try again",
+                    "Ok");
+            }
+        }
+        public class RestService
+        {
+            HttpClient client = new HttpClient();
+            public async Task<PokedexData> GetPokedexDataAsync(string uri)
+            {
+                PokedexData pokedexData = null;
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        pokedexData = PokedexData.FromJson(content);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("\tERROR {0}", ex.Message);
+                }
+                return pokedexData;
+            }
+        }
+    }
+}
+
---
+++ Pokedex/ViewModels/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿namespace Pokedex.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+
+    public class MainViewModel : BaseViewModel
+    {
+        #region Services
+        private APIService apiService;
+        private APIService.RestService restService;
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.apiService = new APIService();
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoadPokemonsCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadPokemons);
+            }
+        }
+
+        private async void LoadPokemons()
+        {
+            restService = new APIService.RestService();
+            APIService.ConectivityStatus();
+
+            PokedexData pokedexData = await restService.GetPokedexDataAsync(GenerateRequestUri(PokemonSourceData));
+
+            PokemonName = pokedexData.Pokemon[0].Name;
+            PokemonImage = string.Join("", "https://www.serebii.net/", pokedexData.Pokemon[0].Img.LocalPath);
+            PokemonType = string.Join(" ", pokedexData.Pokemon[0].Type);
+
+
+            //If the method needs some query parameters or has a KEY. (This is OPTIONAL)
+            string GenerateRequestUri(string endpoint)
+            {
+                string requestUri = endpoint;
+                requestUri += "https://raw.githubusercontent.com/biuni/pokemonGo-pokedex/";
+                requestUri += "master/pokedex.json";
+                return requestUri;
+            }
+        }
+        #endregion
+
+        #region Attributes
+        private string pokemonName;
+        private string pokemonImage;
+        private string pokemontype;
+        public const string PokemonSourceData = "https://raw.githubusercontent.com/biuni/pokemonGo-pokedex/master/pokedex.json";
+        #endregion
+
+        #region Properties
+        public string PokemonName
+        {
+            get { return this.pokemonName; }
+            set { SetValue(ref this.pokemonName, value); }
+        }
+        public string PokemonImage
+        {
+            get { return this.pokemonImage; }
+            set { SetValue(ref this.pokemonImage, value); }
+        }
+        public string PokemonType
+        {
+            get { return this.pokemontype; }
+            set { SetValue(ref this.pokemontype, value); }
+        }
+        #endregion
+    }
+}
